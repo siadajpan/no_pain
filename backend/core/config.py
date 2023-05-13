c3d293ab371cc348eb7ca75bf4d4c96e1e78dbf1
Karol Misiarz <karol.misiarz@robovision.eu>
2023-05-13 17:36:59 +0200

    Add jwt token

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
-    PROJECT_NAME: str = "no pain"
+    PROJECT_NAME: str = "No Pain"
     PROJECT_VERSION: str = "0.1.0"
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
@@ -19,6 +19,10 @@ class Settings:
     )  # default postgres port is 5432
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 settings = Settings()
