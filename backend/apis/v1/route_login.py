bbda8768e42bc74f96a0b48b2ca4cede251ca70e
karol <karol.misiarz@robovision.eu>
2023-05-28 12:26:31 +0200

    Secure JWT token with http only

--- backend/apis/v1/route_login.py
+++ backend/apis/v1/route_login.py
@@ -4,7 +4,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from starlette.responses import Response
 
+from backend.apis.utils import OAuth2PasswordBearerWithCookie
 from backend.core.config import settings
 from backend.core.hashing import Hasher
 from backend.core.security import create_access_token
@@ -24,21 +26,29 @@ def authenticate_user(email: str, password: str, db: Session):
         )
     return user
 
-
 @router.post("/token", response_model=Token)
 def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+        response: Response,
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        db: Session = Depends(get_db)
 ):
+
     user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
 
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
 
 def get_current_user_from_token(
